3264cde97bb697064760170a86984e2dd173036d
Venkatesh <PVenkatesh@ashleyfurniture.com>
2026-04-12 09:48:25 -0500

    Remove scratch/helper files from repo

--- find_port.ps1
+++
@@ -1,7 +0,0 @@
-$used = @(netstat -ano | Select-String ':\d+' | ForEach-Object {
-    if ($_ -match ':(\d+)\s') { [int]$Matches[1] }
-}) | Sort-Object -Unique
-
-foreach ($p in 9100,9200,9300,9400,9500,9600,9700,9800,9900,9999) {
-    if ($p -notin $used) { Write-Host "FREE: $p"; break }
-}
--- find_tdevice.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem -Path 'C:\Users\PVenkatesh\Downloads' -Recurse -Depth 3 -ErrorAction SilentlyContinue |
-    Where-Object { $_.Name -match 't.?device|ear.?tester' } |
-    ForEach-Object { $_.FullName }
